40e27e8d58b7d45f1492f2f93f33f617e977e8d0
DNIIBOY <daniel.nettelfield@gmail.com>
2024-03-20 11:13:47 +0100

    Add bind for Neogit merge

--- .config/nvim/lua/p-neogit.lua
+++ .config/nvim/lua/p-neogit.lua
@@ -5,3 +5,4 @@ vim.keymap.set('n', '<leader>gs', neogit.open, { noremap = true, silent = true }
 vim.keymap.set('n', '<leader>gc', ':Neogit commit<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>gp', ':Neogit pull<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>gP', ':Neogit push<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>gm', ':Neogit merge<CR>', { noremap = true, silent = true })
