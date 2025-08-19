afbaee198f7a92446fca73b56d0582d0a39bf83f
DNIIBOY <daniel@nettelfield.com>
2025-08-19 17:18:54 +0200

    Update some hyprland configs

---
+++ .config/hypr/scripts/random_wallpaper.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+for i in {1..30}; do
+    if pgrep -x hyprpaper > /dev/null; then
+        break
+    fi
+    sleep 0.1
+    if [ $i -eq 30 ]; then
+        echo "Timed out waiting for hyprpaper"
+        exit 1
+    fi
+done
+
+WALLPAPER_DIR="$HOME/Pictures/wallpapers/"
+CURRENT_WALL=$(hyprctl hyprpaper listloaded)
+
+# Get a random wallpaper that is not the current one
+WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" ! -name "empty.png" | shuf -n 1)
+
+# Apply the selected wallpaper
+hyprctl hyprpaper reload ,"$WALLPAPER"
