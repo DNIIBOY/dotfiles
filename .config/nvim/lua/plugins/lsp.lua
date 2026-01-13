aa99382fb3bca9412c9ce5fe17960c3e32ae0ee6
Daniel Nettelfield <daniel@nettelfield.com>
2026-01-13 22:39:22 +0100

    Update tmux to powerkit

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -369,7 +369,6 @@ return {
         "nvim-treesitter/nvim-treesitter",
         event = "BufReadPost",
         build = ":TSUpdate",
-        main = "nvim-treesitter.configs",
         opts = {
             ensure_installed = { "lua", "python" },
             auto_install = true,
